5cfe1f9a54035b4d4689bc1a0c26668db9d7854d
Aditya050806 <aditya.bijwe06@gmail.com>
2026-08-11 02:30:39 +0530

    Update detect.py

--- backend/detect.py
+++ backend/detect.py
@@ -1,54 +1,58 @@
 from ultralytics import YOLO
 import cv2
-import os
 import time
 
+# ==========================
+# LOAD MODEL (ONLY ONCE)
+# ==========================
 model = YOLO("best.pt")
 model.fuse()
+
+
+# ==========================
+# AI DETECTION
+# ==========================
 def analyze_waste(image_path):
 
-   results = model.predict(
-    source=image_path,
-    imgsz=640,
-    conf=0.4,
-    device="cpu",
-    verbose=False
-)
+    results = model.predict(
+        source=image_path,
+        imgsz=640,
+        conf=0.4,
+        device="cpu",
+        verbose=False
+    )
 
-   result = results[0]
+    result = results[0]
 
-names = model.names
+    names = model.names
 
-print("All Classes:", names)   # 👈 ADD HERE
+    print("All Classes:", names)
 
-plastic = 0
-metal = 0
-organic = 0
+    plastic = 0
+    metal = 0
+    organic = 0
 
-total = len(result.boxes)
+    total = len(result.boxes)
 
-for box in result.boxes:
+    for box in result.boxes:
 
-    cls = int(box.cls[0])
+        cls = int(box.cls[0])
+        label = str(names[cls]).lower()
 
-    label = names[cls]
+        print("Detected Label:", label)
 
-    print("Detected Label:", label)   # 👈 ADD HERE
+        if "plastic" in label:
+            plastic += 1
 
-    if label == "plastic":
-        plastic += 1
+        elif "metal" in label:
+            metal += 1
 
-    elif label == "metal":
-        metal += 1
+        elif "organic" in label:
+            organic += 1
 
-    elif label == "organic":
-        organic += 1
     if total > 0:
-
         plastic = round((plastic / total) * 100, 2)
-
         metal = round((metal / total) * 100, 2)
-
         organic = round((organic / total) * 100, 2)
 
     plotted = result.plot()
